d78da95a88d2ad9317fff0941c006be7c14f9373
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-11-14 22:50:33 +0100

    JavaScripting

---
+++ DOMMAN/js/Dom.js
@@ -0,0 +1,19 @@
+//console.log(document.getElementById("heading"));
+//var heading = document.getElementById("heading");
+//heading.innerHTML= "Hibiscus";
+//var par =document.querySelector('p');
+//par.innerHTML = "Otondo";
+//var list = document.getElementById("list");
+//console.log(list);
+//list.innerHTML="Pomengrenate";
+//document.getElementById("heading");
+//console.log(heading.textContent);
+//console.log(heading.innerText)
+//var list =document.getElementsByClassName("list-item");
+//console.log(list[2].innerHTML);
+//list[2].innerHTML="Grape";
+//list[2].style.color="red";
+//var input =document.getElementsByTagName("li");
+//console.log(input);
+var list = document.querySelectorAll('.list-item');
+console.log(list);
