dcc48ed31d173d516c594590f5b1cb5d370782d8
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-11-15 16:36:16 +0100

    Appending

--- DOMMAN/js/Dom.js
+++ DOMMAN/js/Dom.js
@@ -15,5 +15,56 @@
 //list[2].style.color="red";
 //var input =document.getElementsByTagName("li");
 //console.log(input);
-var list = document.querySelectorAll('.list-item');
-console.log(list);
+//var list = document.querySelectorAll('.list-item');
+//console.log(list);
+//var list = document.getElementsByTagName('li');
+//console.log(list);
+//var li1 = list[0];
+//console.log(li1);
+//li1.style.color="yellow";
+//li1.innerHTML = "Banana";
+//var list=document.querySelector('#list');
+//console.log(list.parentNode);
+//console.log(list.parentElement);
+//var body=document.querySelector('#list');
+//console.log(body.childNodes);
+
+//console.log(body.firstChild); 
+//var list = document.querySelector('#list');
+//console.log(list.childNodes); 
+//console.log(list.children[1]);
+//console.log("Hello"); 
+//console.log(list.lastElementChild);
+//console.log(list.previousElementSibling.previousElementSibling.previousElementSibling);
+//console.log(list.previousSibling);
+//console.log(list.nextElementSibling);
+//console.log(list.nextSibling);
+//var h1=document.createElement('h1');
+
+//h1.className="header";
+//h1.id="header1";
+
+//h1.setAttribute('title', 'iam title');
+//var headingText=document.createTextNode
+//("this is my heading");
+//h1.appendChild(headingText);
+//console.log(h1);
+//var newElement=document.getElementById("new");
+//newElement.innerHTML=h1;
+//var body=document.querySelector("body");
+//var input=document.querySelector("input");
+//body.insertBefore(h1,input);
+//var input=document.querySelector("input");
+//input.remove()
+//var body=document.querySelector("body");
+//var button=document.querySelector("button")
+//body.removeChild(button)
+// var para=document.createElement("p");
+// var node=document.createTextNode("This is nouveu.");
+// para.appendChild(node);
+// console.log(para);
+// var body=document.querySelector("body");
+// var p=document.querySelector("p");
+// body.replaceChild(para, p);
+
+
